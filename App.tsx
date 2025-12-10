01507dc3dd1b070b6c6e0ec0b5e338829dad3ffe
zq <zq@czq.auto>
2025-12-10 09:38:30 +0800

    init  react native project

--- App.tsx
+++ App.tsx
@@ -5,11 +5,10 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+
+import {StatusBar, Text, useColorScheme, View} from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 function App() {
@@ -17,29 +16,17 @@ function App() {
 
   return (
     <SafeAreaProvider>
+        <View className="flex flex-row justify-center m-auto items-center w-[100px] h-[100px] bg-red-500">
+            <Text>hello world</Text>
+        </View>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+
     </SafeAreaProvider>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+
 
 export default App;
